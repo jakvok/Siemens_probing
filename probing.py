8cb8eee26e0faa88765468279c57a079161a1e73
jakvok <jakvok@gmail.com>
2022-04-07 19:29:13 +0200

    V1.1.0
    Added czech language mutation

--- probing.py
+++ probing.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
 
+import sys
 class Cycle:
     '''
     Class represents Siemens probing cycles.
+    Probing cycles are used for drive CNC machines measuring probe.
+    The object get all cycle parameters from text input, store them and is able to generate cycle representation in right syntax.
+    The cycles involved in class are CYCLE977 (hole, shaft, groove, web probing) and CYCLE978 (surface probing).
     '''
 
     def __ask(self, variants, text):
+        '''
+        Method ask to choose input from several posibilities.
+        parameters: list of posibilities, explain text
+        return: one of the possibilities choosen
+        '''
         while True:
             print('\n'+text)
             for n in range(len(variants)):
@@ -14,73 +23,136 @@ class Cycle:
                 x = int(input())
                 return variants[x-1]
             except ValueError:
-                print('Set number.')
+                print(self.__langs[5][lang])
             except IndexError:
-                print('Out of range.')
+                print(self.__langs[6][lang])
 
     def __enter_nr(self, text):
+        '''
+        Method for input positive/negative/float/integer number.
+        parameter: explain text
+        return: the number
+        '''
         while 1:
             nr = input('\n'+text)
             if '.' in nr:
                 try:
                     return float(nr)
                 except ValueError:
-                    print('Set number.')
+                    print(self.__langs[5][lang])
             else:
                 try:
                     return int(nr)
                 except ValueError:
-                    print('Set number.')
+                    print(self.__langs[5][lang])
 
     def __enter_positive_nr(self, text):
+        '''
+        Method for input positive/float/integer number.
+        parameter: explain text
+        return: the positive value number
+        '''
         while 1:
             nr = self.__enter_nr(text)
             if nr > 0: return nr
-            print('Must be positive value!')
+            print(self.__langs[7][lang])
 
-    def __init__(self):
+    def __init__(self, lang):
+        '''
+        Method ask for all cycle parameters and initialize cycle object
+        parameter: int number for choose language variant
+        '''
 
-        # set offset?
-        vars = ['Just measure', 'Set offset']
-        a = self.__ask(vars, 'Just measure or set offset?')
+        # language dictionary, involves different languages variant of text used for user communication
+        # 0 = english
+        # 1 = czech
+        self.__langs = [
+            ['surface', 'povrch'], # 0
+            ['hole', 'dira'],
+            ['shaft', 'cep'],
+            ['groove', 'drazka'],
+            ['web', 'zebro'],
+            ['Set number.', 'Zadej cislo.'],
+            ['Out of range.', 'Mimo rozsah.'],
+            ['Must be positive value!', 'Hodnota musi byt vetsi nez 0!'],
+            ['Just measure', 'Pouze mereni'],
+            ['Set offset', 'Posunuti nuloveho bodu'],
+            ['Just measure or set offset?', 'Jen merit nebo zadat nulovy bod?'], # 10
+            ['Enter probing variant', 'Zadej zpusob mereni'],
+            ['Enter offset [G54-57, G505-G599]: G', 'Zadej nulovy bod [G54-57, G505-G599]: G'],
+            ['raw', 'hruby'],
+            ['soft', 'jemny'],
+            ['Say if result values put into \'raw\' or \'soft\' offset table cell:', 'Zvol jestli hodnoty zapisovat do \'hrube\' nebo \'jemne\' tabulky posunuti.'],
+            ['Enter set of calibration data [1-10]:', 'Zadej sadu kalibracnich dat [1-10]:'],
+            ['Enter contingence interval [s]:', 'Zadej kontingencni interval [s]:'],
+            ['Enter measured value [mm]:','Zadej merenou hodnotu [mm]:'],
+            ['Enter travel distance [mm]:','Zadej drahu pohybu mereni [mm]:'],
+            ['Set probing axe:','Zadej osu mereni:'], # 20
+            ['Enter probing direction:','Zadej smer mereni:'],
+            ['hole diameter','prumer diry'],
+            ['shaft diameter','prumer cepu'],
+            ['groove width','sirku drazky'],
+            ['web width','sirku zebra'],
+            ['Enter ','Zadej '],
+            ['Set safety distance [mm]:','Zadej bezpecnostni vzdalenost [mm]:'],
+            ['Enter probing angle between probing direction and axe [deg]:','Zadej uhel mereni mezi smerem mereni a osou [st]:'],
+            ['Probing angle must be in range -359 to 359 deg!','Uhel mereni musi byt v rozmezi +-359 stupnu!'],
+            ['YES','ANO'], #30
+            ['NO','NE'],
+            ['Protection zone?:','Ochranna zona?:'],
+            ['Enter width/diameter of protection zone [mm]:','Zadej sirku/prumer ochranne zony [mm]:'],
+            ['Set dive distance DZ [mm]:','Zadej ponoreni DZ v ose Z [mm]:'],
+            ['Set groove/web center position in axe','Zadej pozici stredu drazky/zebra v ose'],
+            ['Set hole/shaft position in axe X [mm]:','Zadej pozici stredu diry/cepu v ose X [mm]:'],
+            ['Set hole/shaft position in axe Y [mm]:','Zadej pozici stredu diry/cepu v ose Y [mm]:'],
+            ['',''],
+            ['',''],
+        ]
+
+        # Followed cycle parameters are united for both 977 and 978 cycles
+
+        # set offset or just measure value?
+        vars = [self.__langs[8][lang], self.__langs[9][lang]]
+        a = self.__ask(vars, self.__langs[10][lang])
         if a == vars[0]: self.__set_offset = False
         else: self.__set_offset = True
 
-        # probing variant
-        vars = ['surface', 'hole', 'shaft', 'groove', 'web']
-        a = self.__ask(vars, 'Enter probing variant:')
+        # probing variant (surface, hole, shaft, groove, web)
+        vars = [self.__langs[0][lang], self.__langs[1][lang], self.__langs[2][lang], self.__langs[3][lang], self.__langs[4][lang]]
+        a = self.__ask(vars, self.__langs[11][lang])
         for n in vars:
             if a == n: self.__probing_var = a
 
-        # cycle name
-        if self.__probing_var == 'surface': self.__cycle_name = 'CYCLE978'
+        # cycle name comming from probing variant
+        if self.__probing_var == self.__langs[0][lang]: self.__cycle_name = 'CYCLE978'
         else: self.__cycle_name = 'CYCLE977'
 
-        # offset
+        # number of coordinate system offset to where write the measured value G54-G57 and G505-G599 
         if self.__set_offset:
             while 1:
-                self.__offset = self.__enter_nr('Enter offset [G54-57, G505-G599]: G')
+                self.__offset = self.__enter_nr(self.__langs[12][lang])
                 if self.__offset in range(54,58) or self.__offset in range(505, 600): break
+                print(self.__langs[6][lang])
             if self.__offset < 500: self.__offset -= 53
             else: self.__offset -= 500
 
-        # raw / soft
+        # write offset to raw/soft values
         if self.__set_offset:
-            vars = ['raw', 'soft']
-            a = self.__ask(vars, 'Say if result values put into \'raw\' or \'soft\' offset table cell:')
+            vars = [self.__langs[13][lang], self.__langs[14][lang]]
+            a = self.__ask(vars, self.__langs[15][lang])
             if a == vars[0]: self.__raw = True
             else: self.__raw = False
 
-        # set of calibration data
+        # set of the probe calibration data
         while 1:
-            self.__calib_data = (self.__enter_nr('Enter set of calibration data [1-10]:'))
+            self.__calib_data = (self.__enter_nr(self.__langs[16][lang]))
             if int(self.__calib_data) in range(1,11): break
-            print('Out of range.')
+            print(self.__langs[6][lang])
 
         # contingence interval
-        self.__contingence_interval = (self.__enter_positive_nr('Enter contingence interval [s]:'))
+        self.__contingence_interval = (self.__enter_positive_nr(self.__langs[17][lang]))
         
-        # list of all cycle parameters
+        # list of all cycle parameters where the parameters will be stored
         self.__body = []
 
         # static parameters when cycle sets offset
@@ -92,28 +164,29 @@ class Cycle:
 
 
         #----------------  CYCLE978  ------------------------------------
+        # parameters used just in cycle 978
 
         if self.__cycle_name == 'CYCLE978':
             
             # measured value
-            self.__value = self.__enter_nr('Enter measured value [mm]:')
+            self.__value = self.__enter_nr(self.__langs[18][lang])
 
             # travel distance
-            self.__travel_dist = self.__enter_positive_nr('Enter travel distance [mm]:')
+            self.__travel_dist = self.__enter_positive_nr(self.__langs[19][lang])
 
             # probing axis
             vars = ['X', 'Y', 'Z']
-            a = self.__ask(vars, 'Set probing axe:')
+            a = self.__ask(vars, self.__langs[20][lang])
             for n in vars:
                 if n == a: self.__axe = (vars.index(n) + 1)
 
             # probing direction
             vars = ['+', '-']
-            a = self.__ask(vars, 'Enter probing direction:')
+            a = self.__ask(vars, self.__langs[21][lang])
             for n in vars:
                 if n == a: self.__direction = (vars.index(n) + 1)
 
-            # fill parameters
+            # fill parameters into cycle body
             #-----------------
 
             # param 1
@@ -159,65 +232,65 @@ class Cycle:
 
 
         #----------------  CYCLE977  ------------------------------------
+        # parameters used just in cycle 977
 
         if self.__cycle_name == 'CYCLE977':
 
             # measured value
-            if self.__probing_var == 'hole': text = 'hole diameter'
-            elif self.__probing_var == 'shaft': text = 'shaft diameter'
-            elif self.__probing_var == 'groove': text = 'groove width'
-            elif self.__probing_var == 'web': text = 'web width'
+            if self.__probing_var == self.__langs[1][lang]: text = self.__langs[22][lang]
+            elif self.__probing_var == self.__langs[2][lang]: text = self.__langs[23][lang]
+            elif self.__probing_var == self.__langs[3][lang]: text = self.__langs[24][lang]
+            elif self.__probing_var == self.__langs[4][lang]: text = self.__langs[25][lang]
             else: pass
-            self.__value = self.__enter_nr('Enter {} [mm]:'.format(text))
+            self.__value = self.__enter_positive_nr(self.__langs[26][lang]+'{} [mm]:'.format(text))
 
             # safety distance DFA
-            self.__safety_dist = self.__enter_positive_nr('Set safety distance [mm]:')
+            self.__safety_dist = self.__enter_positive_nr(self.__langs[27][lang])
 
             # probing angle
             while 1:
-                self.__probing_angle = self.__enter_nr('Enter probing angle between probing direction and axe [deg]:')
+                self.__probing_angle = self.__enter_nr(self.__langs[28][lang])
                 if int(self.__probing_angle) in range(-359, 360): break
-                print('Probing angle must be in range -359 to 359 deg!')
+                print(self.__langs[29][lang])
 
             # protection zone
-            vars = ['YES', 'NO']
-            a = self.__ask(vars, 'Protection zone?:')
+            vars = [self.__langs[30][lang], self.__langs[31][lang]]
+            a = self.__ask(vars, self.__langs[32][lang])
             if a == vars[0]:
                 self.__protection_zone = True
-                self.__prot_zone_dimm = self.__enter_positive_nr('Enter width/diameter of protection zone [mm]:')
+                self.__prot_zone_dimm = self.__enter_positive_nr(self.__langs[33][lang])
             else: self.__protection_zone = False
 
             # dive distance
-            if self.__probing_var in ['hole', 'groove'] and not self.__protection_zone:
+            if self.__probing_var in [self.__langs[1][lang], self.__langs[3][lang]] and not self.__protection_zone:
                 self.__dive_dist = 1
             else:
-                self.__dive_dist = self.__enter_positive_nr('Set dive distance DZ [mm]:')
+                self.__dive_dist = self.__enter_positive_nr(self.__langs[34][lang])
 
             # probing axe
-            if self.__probing_var in ['groove', 'web']:
+            if self.__probing_var in [self.__langs[3][lang], self.__langs[4][lang]]:
                 vars = ['X', 'Y']
-                a = self.__ask(vars, 'Set probing axe:')
+                a = self.__ask(vars, self.__langs[20][lang])
                 for n in vars:
                     if n == a: self.__axe = vars.index(n) + 1
 
             # center position
-            if self.__set_offset and self.__probing_var in ['groove', 'web']:
-                self.__center = [self.__enter_nr('Set groove/web center position in axe {} [mm]:'.format(chr(87+self.__axe)))]            
-            elif self.__set_offset and self.__probing_var in ['hole', 'shaft']:
-                self.__center = [self.__enter_nr('Set hole/shaft position in axe X [mm]:')]
-                self.__center += [self.__enter_nr('Set hole/shaft position in axe Y [mm]:')]
+            if self.__set_offset and self.__probing_var in [self.__langs[3][lang], self.__langs[4][lang]]:
+                self.__center = [self.__enter_nr(self.__langs[35][lang]+' {} [mm]:'.format(chr(87+self.__axe)))]            
+            elif self.__set_offset and self.__probing_var in [self.__langs[1][lang], self.__langs[2][lang]]:
+                self.__center = [self.__enter_nr(self.__langs[36][lang])]
+                self.__center += [self.__enter_nr(self.__langs[37][lang])]
             else: self.__center = [0, 0]
 
 
-
-            # fill parameters
+            # fill parameters into cycle body
             #-----------------
 
             # param 1
-            if self.__probing_var == 'hole': variant = 1
-            elif self.__probing_var == 'shaft': variant = 2
-            elif self.__probing_var == 'groove': variant = 3
-            elif self.__probing_var == 'web': variant = 4
+            if self.__probing_var == self.__langs[1][lang]: variant = 1
+            elif self.__probing_var == self.__langs[2][lang]: variant = 2
+            elif self.__probing_var == self.__langs[3][lang]: variant = 3
+            elif self.__probing_var == self.__langs[4][lang]: variant = 4
             else: pass
             if self.__set_offset: variant += 100
             if self.__protection_zone: variant += 1000
@@ -251,7 +324,7 @@ class Cycle:
             self.__body.append(self.__probing_angle)
 
             # param 11
-            if self.__probing_var in ['hole', 'groove'] and not self.__protection_zone:
+            if self.__probing_var in [self.__langs[1][lang], self.__langs[3][lang]] and not self.__protection_zone:
                 self.__body.append(1)
             else: self.__body.append(self.__dive_dist)
 
@@ -284,21 +357,35 @@ class Cycle:
  
 
     def __str__(self) -> str:
+        '''
+        Method for text representation of probing cycle in right syntax
+        '''
         x = ''
         for n in self.__body:
             x += str(n)+','
         return '{0}({1})'.format(self.__cycle_name, x[:(len(x)-1)])
 
 
+langs = [
+    ['| Siemens probing cycles for Grob 350 |','|  Siemens merici cykly pro Grob 350  |'],
+    ['Continue? Y/N:','Pokracovat? A/N:'],
+]
+
+# choose language
+lang = 0 # default = english
+if len(sys.argv) > 1:
+    if sys.argv[1] == 'cz': lang = 1
+
 print('''\n
 +-------------------------------------+
-| Siemens probing cycles for Grob 350 |
-|              v1.0.0                 |
+{}
+|              v1.1.0                 |
 |          by jakvok 2022             |
-+-------------------------------------+''')
++-------------------------------------+'''.format(langs[0][lang]))
 
+# main loop
 while 1:
-    x = Cycle()
+    x = Cycle(lang)
     print('\n{}\n'.format(x))
-    a = input('Continue? Y/N:')
+    a = input(langs[1][lang])
     if a in 'Nn': break
